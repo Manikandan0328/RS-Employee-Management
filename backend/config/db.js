b8712234377f49cd3c3922f54084ee880cce5e7d
Manikandan S <125672983+Manikandan0328@users.noreply.github.com>
2025-09-08 13:24:54 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-
+//root is a user && password change your DB_name and password 
 const sequelize = new Sequelize("rs_tech", "root", "root", {
   host: "localhost",
   dialect: "mysql",
@@ -11,3 +11,4 @@ sequelize
   .catch(err => console.error("MySQL error:", err));
 
 module.exports = sequelize;
+
